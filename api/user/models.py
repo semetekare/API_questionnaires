8ebe31935d3a4b1e5ab3d074e0368b8609325ffd
prokopchuk va <SkiprDAG@yandex.ru>
2023-11-20 21:58:18 +0800

    refactor

--- api/user/models.py
+++ api/user/models.py
@@ -10,28 +10,30 @@ class CustomUser(models.Model):
         ADMIN = 2, _('Админ')
         TEACHER = 3, _('Преподаватель')
 
-    role = models.CharField(max_length=255, choices=Type.choices)
+    login = models.CharField(max_length=128, null=False, blank=False)
+    password = models.TextField(null=False, blank=False)
+    role = models.CharField(max_length=255, choices=Type.choices, null=False, blank=False)
 
 
 class Teacher(models.Model):
-    user = models.ForeignKey('user.CustomUser', on_delete=models.CASCADE)
-    university = models.ForeignKey('user.University', on_delete=models.DO_NOTHING)
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
-    middle_name = models.CharField(max_length=128)
-    description = models.TextField()
-    mail = models.CharField(max_length=128)
-    phone = models.CharField(max_length=10)
-    tg_username = models.CharField(max_length=64)
+    university = models.ForeignKey('user.University', on_delete=models.DO_NOTHING, null=False, blank=False)
+    user = models.ForeignKey('user.CustomUser', on_delete=models.DO_NOTHING, null=False, blank=False)
+    first_name = models.CharField(max_length=128, null=False, blank=False)
+    last_name = models.CharField(max_length=128, null=False, blank=False)
+    middle_name = models.CharField(max_length=128, null=False, blank=False)
+    description = models.TextField(default=None)
+    mail = models.CharField(max_length=128, null=False, blank=False)
+    phone = models.CharField(max_length=10, default=None)
+    tg_username = models.CharField(max_length=64, default=None)
 
 
 class Admin(models.Model):
-    user = models.ForeignKey('user.CustomUser', on_delete=models.CASCADE)
-    university = models.ForeignKey('user.University', on_delete=models.DO_NOTHING)
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
-    middle_name = models.CharField(max_length=128)
+    user = models.ForeignKey('user.CustomUser', on_delete=models.DO_NOTHING, null=False, blank=False)
+    university = models.ForeignKey('user.University', on_delete=models.DO_NOTHING, null=False, blank=False)
+    first_name = models.CharField(max_length=128, null=False, blank=False)
+    last_name = models.CharField(max_length=128, null=False, blank=False)
+    middle_name = models.CharField(max_length=128, null=False, blank=False)
 
 
 class University(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=False, blank=False)
